67232b19ed3310aa92ba26f0430cd734d5b02489
jiylee <jjenny4711@gmail.com>
2024-04-06 01:48:54 -0400

    test1

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 <Route path='/' element={<AppLayout/>}>
 <Route index element={<Home/>}/>
 
-<Route path="movies">
+<Route path="/movies">
 <Route index element={<Movies/>}/>
 <Route path=':id' element={<MovieDetail/>}/>
 </Route>
